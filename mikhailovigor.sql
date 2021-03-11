13f368cefc9ceefe56ae416c086c2f2260a33526
Faradej123 <myhailovigor@gmail.com>
2021-03-11 15:10:37 +0200

    111

--- mikhailovigor.sql
+++ mikhailovigor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 08 2021 г., 21:49
+-- Время создания: Мар 11 2021 г., 15:09
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.1.33
 
@@ -36,6 +36,25 @@ CREATE TABLE `product` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `role_list`
+--
+
+CREATE TABLE `role_list` (
+  `id` int NOT NULL,
+  `title` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `role_list`
+--
+
+INSERT INTO `role_list` (`id`, `title`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `user`
 --
 
@@ -44,15 +63,16 @@ CREATE TABLE `user` (
   `firstname` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
-  `date_created` int NOT NULL
+  `date_created` int NOT NULL,
+  `role_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `firstname`, `email`, `password`, `date_created`) VALUES
-(1, 'Igor', 'qwe@qwe.ru', '202cb962ac59075b964b07152d234b70', 1615232290);
+INSERT INTO `user` (`id`, `firstname`, `email`, `password`, `date_created`, `role_id`) VALUES
+(10, 'Игорь', 'mikhailov.i@gmail.com', '202cb962ac59075b964b07152d234b70', 1615467421, 2);
 
 --
 -- Индексы сохранённых таблиц
@@ -62,6 +82,12 @@ INSERT INTO `user` (`id`, `firstname`, `email`, `password`, `date_created`) VALU
 -- Индексы таблицы `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `role_list`
+--
+ALTER TABLE `role_list`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -81,10 +107,16 @@ ALTER TABLE `product`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT для таблицы `role_list`
+--
+ALTER TABLE `role_list`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
